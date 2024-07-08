6bc0148b116c67f4c801675b2d0143d5ace22e02
yu-heejin <jkgt0521@naver.com>
2024-07-08 12:59:22 +0900

    chore: Request test를 위한 JPA 삭제

--- src/main/kotlin/org/team/coucoudas/CoucoudasServerApplication.kt
+++ src/main/kotlin/org/team/coucoudas/CoucoudasServerApplication.kt
@@ -4,12 +4,12 @@ import jakarta.annotation.PostConstruct
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 import java.util.*
 
 @SpringBootApplication
-@EnableJpaAuditing
+//@EnableJpaAuditing
 class CoucoudasServerApplication
 
 @PostConstruct
